291d083065d678122d3ecb5fb85b10d322d01ddd
riyadh <mrriyadh66@gmail.com>
2022-11-30 16:03:39 +0600

    mui added with header and sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import PrimarySearchAppBar from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <div className="text-[34px] bg-red-500">this is very test one</div>
+      <PrimarySearchAppBar />
+      {/* <div className="text-[34px] bg-red-500">this is very test one</div> */}
     </div>
   );
 }
